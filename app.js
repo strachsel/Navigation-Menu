0ee9c891d059a93993a92f50135b471ea1af23ba
Sonya Trachsel <strachsel@Rubicons-MacBook-Pro.local>
2015-12-06 12:49:34 -0800

    Closing the element
    When you click the X icon, the menu pushes back to the right.

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ $('body').animate({
     left: '285px'
     }, 200);  
 } );
+//closing the element
+$('.icon-close').click(function(){
+    $('.menu').animate({
+        left:'-285px'
+     }, 200);
+    
+    $('body').animate({
+         left: '0px'
+    }, 200);  
+  });
+};
+$(document).ready(main);
