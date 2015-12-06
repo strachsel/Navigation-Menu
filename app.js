6f37bfe7c03f9565262590c016bb2cca0cdae72b
Sonya Trachsel <strachsel@Rubicons-MacBook-Pro.local>
2015-12-06 12:49:06 -0800

    Push menu icon
    Push menu icon from the left

---
+++ app.js
@@ -0,0 +1,11 @@
+var main = function() {
+    
+$('.icon-menu').click(function(){
+ $('.menu').animate({
+    left: '0px' 
+     }, 200);
+   
+$('body').animate({
+    left: '285px'
+    }, 200);  
+} );
